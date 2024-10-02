3209fc21fc39c1150933c65bd7c7bda485ad1334
Amit Pandit <1amitpandit2468@gmail.com>
2024-10-02 12:46:48 +0530

    changed deployment to dynamic

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-    	domains: ['substackcdn.com'],
+    	domains: ['substackcdn.com'], // Add the domain here
+    	unoptimized: true,
   	},
   	assetPrefix: './',
 };
