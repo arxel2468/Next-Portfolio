8fa0a0ef15a148ebbc3c18c50995dae1936b062e
Amit Pandit <1amitpandit2468@gmail.com>
2025-07-17 00:19:23 +0530

    Update Hero.js

--- my-portfolio/src/components/sections/Hero.js
+++ my-portfolio/src/components/sections/Hero.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo} from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
